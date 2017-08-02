ad36054662df00bfaa1f13d9943e2196a7ddf814
dramos <dario.fenix@gmail.com>
2017-08-02 18:23:40 -0300

    DEV-8: Lapsus

--- build.py
+++ build.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     builder.add_common_builds(shared_option_name="zlib:shared", pure_c=True)
     builder.password = os.getenv("CONAN_PASSWORD")
     filtered_builds = []
-        for settings, options, env_vars, build_requires in builder.builds:
-            if settings["build_type"] == "Release" \
-                and settings["arch"] == "x86_64" \
-                and not options["zlib:shared"]:
-                filtered_builds.append([settings, options, env_vars, build_requires])
+    for settings, options, env_vars, build_requires in builder.builds:
+        if settings["build_type"] == "Release" \
+               and settings["arch"] == "x86_64" \
+               and not options["zlib:shared"]:
+            filtered_builds.append([settings, options, env_vars, build_requires])
     builder.builds = filtered_builds
     builder.run()
