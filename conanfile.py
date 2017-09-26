87d99548a644e47bab0c485020ac3708b2878f3b
Dario Ramos <dario.fenix@gmail.com>
2017-09-26 14:31:44 -0300

    DEV-31: SSL error fix when building from source

--- conanfile.py
+++ conanfile.py
@@ -43,7 +43,7 @@ class ZlibConan(ConanFile):
 
     def source(self):
         zip_name = "zlib-%s.tar.gz" % self.version
-        tools.download("http://downloads.sourceforge.net/project/libpng/zlib/%s/%s" % (self.version, zip_name), zip_name)
+        tools.download("https://downloads.sourceforge.net/project/libpng/zlib/%s/%s" % (self.version, zip_name), zip_name)
         tools.unzip(zip_name)
         os.unlink(zip_name)
         files.rmdir("%s/contrib" % self.ZIP_FOLDER_NAME)
